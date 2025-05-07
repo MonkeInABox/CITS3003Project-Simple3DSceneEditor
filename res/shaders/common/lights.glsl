86157eed741c7924043d17b7636ca082fc90513a
Jeremy Butson <108125978+MonkeInABox@users.noreply.github.com>
2025-05-07 13:46:13 +0800

    cream

--- res/shaders/common/lights.glsl
+++ res/shaders/common/lights.glsl
@@ -2,6 +2,11 @@
 #define NUM_PL 0
 #endif
 
+#ifndef NUM_DL
+#define NUM_DL 0
+#endif
+
+
 // Material Properties
 struct Material {
     vec3 diffuse_tint;
@@ -21,6 +26,13 @@ struct PointLightData {
     vec3 position;
     vec3 colour;
     vec3 attenuation_factors;
+};
+
+//Directioanl loigtht dater
+struct DirectionalLightData {
+    vec3 colour;
+    vec3 attenuation_factors;
+    vec3 facing_direction;
 };
 
 // Calculations
@@ -51,6 +63,25 @@ void point_light_calculation(PointLightData point_light, LightCalculatioData cal
     total_ambient += ambient_component;
 }
 
+// Directional Lights
+void directional_light_calculation(DirectionalLightData directional_light, LightCalculatioData calculation_data, float shininess, inout vec3 total_diffuse, inout vec3 total_specular, inout vec3 total_ambient) {
+    // Ambient
+    vec3 ambient_component = ambient_factor * directional_light.colour;
+
+    // Diffuse
+    float diffuse_factor = max(dot(directional_light.facing_direction, calculation_data.ws_normal), 0.0f);
+    vec3 diffuse_component = diffuse_factor * directional_light.colour;
+
+    // Specular
+    vec3 ws_halfway_dir = normalize(directional_light.facing_direction + calculation_data.ws_view_dir);
+    float specular_factor = pow(max(dot(calculation_data.ws_normal, ws_halfway_dir), 0.0f), shininess);
+    vec3 specular_component = specular_factor * directional_light.colour;
+
+    total_diffuse += diffuse_component;
+    total_specular += specular_component;
+    total_ambient += ambient_component;
+}
+
 // Total Calculation
 
 struct LightingResult {
@@ -63,6 +94,9 @@ LightingResult total_light_calculation(LightCalculatioData light_calculation_dat
         #if NUM_PL > 0
         ,PointLightData point_lights[NUM_PL]
         #endif
+        #if NUM_DL > 0
+        ,PointLightData point_lights[NUM_DL]
+        #endif
     ) {
 
     vec3 total_diffuse = vec3(0.0f);
@@ -74,9 +108,17 @@ LightingResult total_light_calculation(LightCalculatioData light_calculation_dat
         point_light_calculation(point_lights[i], light_calculation_data, material.shininess, total_diffuse, total_specular, total_ambient);
     }
     #endif
+    #if NUM_DL > 0
+    for (int i = 0; i < NUM_DL; i++) {
+        directional_light_calculation(directional_lights[i], light_calculation_data, material.shininess, total_diffuse, total_specular, total_ambient);
+    }
+    #endif
 
     #if NUM_PL > 0
     total_ambient /= float(NUM_PL);
+    #endif
+    #if NUM_DL > 0
+    total_ambient /= float(NUM_DL);
     #endif
 
     total_diffuse *= material.diffuse_tint;
