734f07ef853df58361716d4d865b61ccdee9253a
SwordOfTheNebulae <49628441+SwordOfTheNebulae@users.noreply.github.com>
2025-05-07 11:22:11 +0800

    shading

--- res/shaders/entity/vert.glsl
+++ res/shaders/entity/vert.glsl
@@ -1,5 +1,5 @@
 #version 410 core
-#include "../common/lights.glsl"
+// #include "../common/lights.glsl"
 
 // Per vertex data
 layout(location = 0) in vec3 vertex_position;
@@ -7,7 +7,9 @@ layout(location = 1) in vec3 normal;
 layout(location = 2) in vec2 texture_coordinate;
 
 out VertexOut {
-    LightingResult lighting_result;
+    vec3 position;
+    vec3 view_dir;
+    vec3 normal;
     vec2 texture_coordinate;
 } vertex_out;
 
@@ -15,18 +17,18 @@ out VertexOut {
 uniform mat4 model_matrix;
 uniform mat3 normal_matrix;
 
-// Material properties
-uniform vec3 diffuse_tint;
-uniform vec3 specular_tint;
-uniform vec3 ambient_tint;
-uniform float shininess;
+// // Material properties
+// uniform vec3 diffuse_tint;
+// uniform vec3 specular_tint;
+// uniform vec3 ambient_tint;
+// uniform float shininess;
 
-// Light Data
-#if NUM_PL > 0
-layout (std140) uniform PointLightArray {
-    PointLightData point_lights[NUM_PL];
-};
-#endif
+// // Light Data
+// #if NUM_PL > 0
+// layout (std140) uniform PointLightArray {
+//     PointLightData point_lights[NUM_PL];
+// };
+// #endif
 
 // Global data
 uniform vec3 ws_view_position;
@@ -35,19 +37,20 @@ uniform mat4 projection_view_matrix;
 void main() {
     // Transform vertices
     vec3 ws_position = (model_matrix * vec4(vertex_position, 1.0f)).xyz;
-    vec3 ws_normal = normalize(normal_matrix * normal);
+    vertex_out.position = ws_position;
+    vertex_out.normal = normalize(normal_matrix * normal);
     vertex_out.texture_coordinate = texture_coordinate;
 
     gl_Position = projection_view_matrix * vec4(ws_position, 1.0f);
 
     // Per vertex lighting
-    vec3 ws_view_dir = normalize(ws_view_position - ws_position);
-    LightCalculatioData light_calculation_data = LightCalculatioData(ws_position, ws_view_dir, ws_normal);
-    Material material = Material(diffuse_tint, specular_tint, ambient_tint, shininess);
+    vertex_out.view_dir = normalize(ws_view_position - ws_position);
+    // LightCalculatioData light_calculation_data = LightCalculatioData(ws_position, ws_view_dir, ws_normal);
+    // Material material = Material(diffuse_tint, specular_tint, ambient_tint, shininess);
 
-    vertex_out.lighting_result = total_light_calculation(light_calculation_data, material
-        #if NUM_PL > 0
-        ,point_lights
-        #endif
-    );
+    // vertex_out.lighting_result = total_light_calculation(light_calculation_data, material
+    //     #if NUM_PL > 0
+    //     ,point_lights
+    //     #endif
+    // );
 }
